cf035728fb74c37351424639777bbbb0c089dd23
Niklas Semmler <niklas.semmler@mailbox.org>
2022-02-18 00:25:49 +0100

    simplify code

--- controllers/logstash_controller.go
+++ controllers/logstash_controller.go
@@ -24,7 +24,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -33,6 +33,7 @@ import (
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	logstashv1alpha1 "github.com/hendrikkiedrowski/logstash-operator-go/api/v1alpha1"
+	"github.com/prometheus/common/log"
 )
 
 // LogstashReconciler reconciles a Logstash object
@@ -41,12 +42,102 @@ type LogstashReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+func (r *LogstashReconciler) fetchLogstash(ctx context.Context, namespace types.NamespacedName) (*logstashv1alpha1.Logstash, *ctrl.Result, error) {
+	log := ctrllog.FromContext(ctx)
+
+	logstash := new(logstashv1alpha1.Logstash)
+	err := r.Get(ctx, namespace, logstash)
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			log.Info("Logstash resource not found. Ignoring since object must be deleted")
+			return nil, &ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get Logstash - requeuing")
+		return nil, &ctrl.Result{}, err
+	}
+	return logstash, nil, nil
+}
+
+func (r *LogstashReconciler) fetchOrCreateStatefulSet(ctx context.Context, logstash *logstashv1alpha1.Logstash) (*appsv1.StatefulSet, *ctrl.Result, error) {
+	log := ctrllog.FromContext(ctx)
+	namespace := types.NamespacedName{Name: logstash.Name, Namespace: logstash.Namespace}
+
+	var foundSfs *appsv1.StatefulSet
+	err := r.Get(ctx, namespace, foundSfs)
+
+	if err != nil && k8serrors.IsNotFound(err) {
+		// Define a new stateful set
+		newSfs := r.statefulsetForLogstash(logstash)
+		log.Info("Creating a new Stateful Set", "StatefulSet.Namespace", newSfs.Namespace, "StatefulSet.Name", newSfs.Name)
+		err = r.Create(ctx, newSfs)
+		if err != nil {
+			log.Error(err, "Failed to create new StatefulSet", "StatefulSet.Namespace", newSfs.Namespace, "StatefulSet.Name", newSfs.Name)
+			return nil, &ctrl.Result{}, err
+		}
+		// Stateful Set created successfully - return and requeue
+		return nil, &ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Stateful Set")
+		return nil, &ctrl.Result{}, err
+
+	}
+	return foundSfs, nil, nil
+}
+
+func (r *LogstashReconciler) updateStatefulSet(ctx context.Context, logstash *logstashv1alpha1.Logstash, sfs *appsv1.StatefulSet) (*ctrl.Result, error) {
+	replicaCount := logstash.Spec.ReplicaCount
+	if *sfs.Spec.Replicas != replicaCount {
+		sfs.Spec.Replicas = &replicaCount
+		err := r.Update(ctx, sfs)
+		if err != nil {
+			log.Error(err, "Failed to update Stateful Set", "StatefulSet.Namespace", sfs.Namespace, "StatefulSet.Name", sfs.Name)
+			return &ctrl.Result{}, err
+		}
+		// Ask to requeue after 1 minute in order to give enough time for the
+		// pods be created on the cluster side and the operand be able
+		// to do the next update step accurately.
+		return &ctrl.Result{RequeueAfter: time.Minute}, nil
+	}
+	return nil, nil
+}
+
+func (r *LogstashReconciler) findLogstashStateChanges(ctx context.Context, logstash *logstashv1alpha1.Logstash) (bool, *ctrl.Result, error) {
+	// Update the Logstash status with the pod names
+	// List the pods for this logstash's stateful set
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(logstash.Namespace),
+		client.MatchingLabels(labelsForLogstash(logstash.Name)),
+	}
+	if err := r.List(ctx, podList, listOpts...); err != nil {
+		log.Error(err, "Failed to list pods", "Logstash.Namespace", logstash.Namespace, "Logstash.Name", logstash.Name)
+		return false, &ctrl.Result{}, err
+	}
+
+	podNames := getPodNames(podList.Items)
+
+	// Update status.Nodes if needed
+	if !reflect.DeepEqual(podNames, logstash.Status.Nodes) {
+		logstash.Status.Nodes = podNames
+		return true, nil, nil
+	}
+	return false, nil, nil
+}
+
+func (r *LogstashReconciler) updateLogstashState(ctx context.Context, logstash *logstashv1alpha1.Logstash) (*ctrl.Result, error) {
+	err := r.Status().Update(ctx, logstash)
+	if err != nil {
+		log.Error(err, "Failed to update Logstash status")
+		return &ctrl.Result{}, err
+	}
+	return nil, nil
+}
+
 //+kubebuilder:rbac:groups=logstash.vkiedrowski.de,resources=logstashes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=logstash.vkiedrowski.de,resources=logstashes/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=logstash.vkiedrowski.de,resources=logstashes/finalizers,verbs=update
 //+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
@@ -57,79 +148,30 @@ type LogstashReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *LogstashReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := ctrllog.FromContext(ctx)
-
-	// Fetch the Logstash instance
-	logstash := &logstashv1alpha1.Logstash{}
-	err := r.Get(ctx, req.NamespacedName, logstash)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
-			// Return and don't requeue
-			log.Info("Logstash resource not found. Ignoring since object must be deleted")
-			return ctrl.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		log.Error(err, "Failed to get Logstash")
-		return ctrl.Result{}, err
+	logstash, returnResult, err := r.fetchLogstash(ctx, req.NamespacedName)
+	if returnResult != nil {
+		return *returnResult, err
 	}
 
-	// Check if the stateful set already exists, if not create a new one
-	found := &appsv1.StatefulSet{}
-	err = r.Get(ctx, types.NamespacedName{Name: logstash.Name, Namespace: logstash.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		// Define a new stateful set
-		sfs := r.statefulsetForLogstash(logstash)
-		log.Info("Creating a new Stateful Set", "StatefulSet.Namespace", sfs.Namespace, "StatefulSet.Name", sfs.Name)
-		err = r.Create(ctx, sfs)
-		if err != nil {
-			log.Error(err, "Failed to create new StatefulSet", "StatefulSet.Namespace", sfs.Namespace, "StatefulSet.Name", sfs.Name)
-			return ctrl.Result{}, err
-		}
-		// Stateful Set created successfully - return and requeue
-		return ctrl.Result{Requeue: true}, nil
-	} else if err != nil {
-		log.Error(err, "Failed to get Stateful Set")
-		return ctrl.Result{}, err
+	sfs, returnResult, err := r.fetchOrCreateStatefulSet(ctx, logstash)
+	if returnResult != nil {
+		return *returnResult, err
 	}
 
-	// Ensure the stateful set replicaCount is the same as the spec
-	replicaCount := logstash.Spec.ReplicaCount
-	if *found.Spec.Replicas != replicaCount {
-		found.Spec.Replicas = &replicaCount
-		err = r.Update(ctx, found)
-		if err != nil {
-			log.Error(err, "Failed to update Stateful Set", "StatefulSet.Namespace", found.Namespace, "StatefulSet.Name", found.Name)
-			return ctrl.Result{}, err
-		}
-		// Ask to requeue after 1 minute in order to give enough time for the
-		// pods be created on the cluster side and the operand be able
-		// to do the next update step accurately.
-		return ctrl.Result{RequeueAfter: time.Minute}, nil
+	returnResult, err = r.updateStatefulSet(ctx, logstash, sfs)
+	if returnResult != nil {
+		return *returnResult, err
 	}
 
-	// Update the Logstash status with the pod names
-	// List the pods for this logstash's stateful set
-	podList := &corev1.PodList{}
-	listOpts := []client.ListOption{
-		client.InNamespace(logstash.Namespace),
-		client.MatchingLabels(labelsForLogstash(logstash.Name)),
-	}
-	if err = r.List(ctx, podList, listOpts...); err != nil {
-		log.Error(err, "Failed to list pods", "Logstash.Namespace", logstash.Namespace, "Logstash.Name", logstash.Name)
-		return ctrl.Result{}, err
+	hasChanged, returnResult, err := r.findLogstashStateChanges(ctx, logstash)
+	if returnResult != nil {
+		return *returnResult, err
 	}
 
-	podNames := getPodNames(podList.Items)
-
-	// Update status.Nodes if needed
-	if !reflect.DeepEqual(podNames, logstash.Status.Nodes) {
-		logstash.Status.Nodes = podNames
-		err := r.Status().Update(ctx, logstash)
-		if err != nil {
-			log.Error(err, "Failed to update Logstash status")
-			return ctrl.Result{}, err
+	if hasChanged {
+		returnResult, err = r.updateLogstashState(ctx, logstash)
+		if returnResult != nil {
+			return *returnResult, err
 		}
 	}
 
